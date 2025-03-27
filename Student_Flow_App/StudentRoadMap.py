0570299444fe2359a9bfdc065b2108f5c4f5e123
tpsdevlop <developers@thoughtprocess.co.in>
2025-03-27 11:11:37 +0530

    27-03-25 2

--- Student_Flow_App/StudentRoadMap.py
+++ Student_Flow_App/StudentRoadMap.py
@@ -42,7 +42,7 @@ def fetch_top_navigation(request,student_id):
 @api_view(['GET'])
 def fetch_roadmap(request,student_id,course_id,subject_id):
     try:
-        blob_data = json.loads(get_blob('LMS_DayWise/Course0001.json'))
+        blob_data = json.loads(get_blob(f'LMS_DayWise/{course_id}.json'))
         course = courses.objects.get(course_id=course_id)
         sub = subjects.objects.get(subject_id = subject_id,del_row = False)
         course_details = list(course_plan_details.objects.filter(course_id=course, subject_id=sub, del_row=False)
@@ -65,10 +65,17 @@ def fetch_roadmap(request,student_id,course_id,subject_id):
         daynumber=0
         for i in course_details:
             week_data = sub_data.get('week_'+str(i.get('week')),{})
-
-            for d in blob_data.get(sub.subject_name): 
+            if i.get('week') > 1:
+                prev_week_data = sub_data.get('week_'+str(i.get('week')-1),{})
+            else:
+                prev_week_data = {}
+            week_first_day = 0
+            for d in blob_data.get(sub.subject_name):
                 the_date = datetime.strptime(d.get('date').replace('T',' ').split('.')[0].replace('Z',''), "%Y-%m-%d %H:%M:%S")
                 if i.get('startDate').date() <= the_date.date() and the_date.date() <= i.get('endDate').date():
+                    if week_first_day == 0:
+                        week_first_day = int(d.get('day').split(' ')[-1]) 
+                        print('week_first_day',week_first_day,"prev_week_data",prev_week_data)
                     day_data = week_data.get('day_'+str(d.get('day').split(' ')[-1]),{})
                     status = ''
                     mcq_qns =len(day_data.get('mcq_questions',[]))
@@ -80,12 +87,19 @@ def fetch_roadmap(request,student_id,course_id,subject_id):
                     elif mcq_answered > 0 or coding_answered > 0 or day_data != {}:
                         status = 'Resume'
                     else:
-                        prev_day_data = week_data.get('day_'+str(daynumber),{})
+                        prev_day_data = week_data.get('day_'+str(int(d.get('day').split(' ')[-1])-1),{})
                         prev_mcq_qns =len(prev_day_data.get('mcq_questions',[]))
                         prev_coding_qns =  len(prev_day_data.get('coding_questions',[]))
                         prev_mcq_answered = len([dd for dd in prev_day_data.get('mcq_questions_status',{}) if prev_day_data.get('mcq_questions_status',{}).get(dd)==2])
                         prev_coding_answered = len([dd for dd in prev_day_data.get('coding_questions_status',{}) if prev_day_data.get('coding_questions_status',{}).get(dd)==2])
                         if prev_mcq_qns == prev_mcq_answered and prev_coding_qns == prev_coding_answered and prev_mcq_qns > 0 and prev_coding_qns > 0:
+                            status = 'Start'
+                        last_weeks_last_day_data = prev_week_data.get('day_'+str(week_first_day-1),{})
+                        last_weeks_mcq_qns =len(last_weeks_last_day_data.get('mcq_questions',[]))
+                        last_weeks_coding_qns =  len(last_weeks_last_day_data.get('coding_questions',[]))
+                        last_weeks_mcq_answered = len([dd for dd in last_weeks_last_day_data.get('mcq_questions_status',{}) if last_weeks_last_day_data.get('mcq_questions_status',{}).get(dd)==2])
+                        last_weeks_coding_answered = len([dd for dd in last_weeks_last_day_data.get('coding_questions_status',{}) if last_weeks_last_day_data.get('coding_questions_status',{}).get(dd)==2])
+                        if (status == '' and daynumber == 0 ) or (last_weeks_mcq_qns == last_weeks_mcq_answered and last_weeks_coding_qns == last_weeks_coding_answered and last_weeks_mcq_qns > 0 and last_weeks_coding_qns > 0):
                             status = 'Start'
                     if d.get('topic') == 'Weekly Test':# or d.get('topic') == 'Onsite Workshop' or d.get('topic') == 'Internship':
                         days.append({'day':daynumber+1,'day_key':d.get('day').split(' ')[-1],
